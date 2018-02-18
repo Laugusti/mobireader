22c4982a60862dee6445273ff92bdab8e2c31d2c
Leon <laugusti@mail.usf.edu>
2018-02-18 10:39:00 -0500

    updated command to print text records of the mobi file

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
+	"golang.org/x/net/html"
+
+	"github.com/Laugusti/gopl/ch5/exercise_5_7/htmlprint"
 	"github.com/Laugusti/mobireader"
 )
 
@@ -13,13 +17,31 @@ var filename = flag.String("filename", "data.mobi", "name of mobi file")
 
 func main() {
 	flag.Parse()
+
+	// open file
 	file, err := os.Open(*filename)
 	if err != nil {
 		log.Fatalf("failed to open file: %v", err)
 	}
+
+	// create MOBIFile from file
 	mobi, err := mobireader.Create(file)
 	if err != nil {
 		log.Fatalf("failed to open file: %v", err)
 	}
-	fmt.Print(mobi.Text())
+
+	// get text from MOBIFile
+	data, err := mobi.Text()
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+
+	// parse text as html
+	node, err := html.Parse(strings.NewReader(data))
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+
+	// pretty print html
+	fmt.Println(htmlprint.PrettyPrint(node))
 }
